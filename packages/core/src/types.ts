179fd68b1b297e3cca0a1cadf05f46772cdec702
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-11-28 17:12:23 +0800

    feat(core): create Config declaration

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -1,2 +1,31 @@
 export type LogLevel = 'trace' | 'debug' | 'info' | 'notice' | 'warn' | 'error';
 export type LogLevelExtended = LogLevel | 'silent';
+
+export interface PluginConfigRegistry { }
+export interface ServiceConfigRegistry { }
+
+export type Config = {
+    project: {
+        name: string;
+    };
+    log?: {
+        loglevel?: LogLevelExtended;
+        logdir?: string;
+    };
+    plugin: {
+        scopes?: string[];
+        directories?: string[];
+        files?: string[];
+        config?: {
+            [key in keyof PluginConfigRegistry]?: PluginConfigRegistry[key];
+        };
+    };
+    service: {
+        scopes?: string[];
+        directories?: string[];
+        files?: string[];
+        config?: {
+            [key in keyof ServiceConfigRegistry]?: ServiceConfigRegistry[key];
+        };
+    }
+}
