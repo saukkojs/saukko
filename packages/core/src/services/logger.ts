ecb459e672723af639635a042b1c06cb3d9db66a
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-06 12:23:10 +0800

    refa(core): do not read config in logger
    avoid circular dependency

--- packages/core/src/services/logger.ts
+++ packages/core/src/services/logger.ts
@@ -1,17 +1,13 @@
 import log from '@cocotais/logger';
 import { LogLevel } from '../types';
-import { ConfigService } from './config';
 
 export class LoggerService {
-    static inject = ['config'];
-    constructor(private config: ConfigService) {}
+    constructor() {}
 
     log(name: string, level: LogLevel, ...messages: any[]) {
         log(name, level, {
             hasDate: true,
-            toConsole: true,
-            toFile: this.config.get('log.logdir'),
-            loglevel: this.config.get('log.loglevel')
+            toConsole: true
         }, ...messages);
     }
 }
