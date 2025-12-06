309699a0e5529cd00494b46fddef3d2807d72218
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-06 12:39:06 +0800

    feat(cli): ensure path by env

--- packages/saukko/src/index.ts
+++ packages/saukko/src/index.ts
@@ -8,6 +8,8 @@ import net from 'net';
 import 'dotenv/config';
 
 const env = process.env as SaukkoEnv;
+const configPath = env.SAUKKO_CONFIG_PATH || path.join(process.cwd(), 'saukko.toml');
+const socketPath = env.SAUKKO_SOCKET_PATH || path.join(process.cwd(), '.saukko.sock');
 
 const logger = new Logger("cli", {
     hasDate: true,
@@ -16,19 +18,19 @@ const logger = new Logger("cli", {
 
 logger.info(`saukko v${version} by Cocotais Team`);
 
-if (fs.existsSync(path.join(process.cwd(), 'saukko.toml'))) {
+if (fs.existsSync(configPath) === false) {
     logger.error('未找到 saukko.toml 文件。');
     logger.notice('你可能需要使用 create-saukko 创建一个项目。');
     logger.notice('请参阅 使用文档 获取更多信息。');
     process.exit(1);
 }
 
-const config = toml.parse(fs.readFileSync(path.join(process.cwd(), 'saukko.toml'), 'utf8'));
+const config = toml.parse(fs.readFileSync(configPath, 'utf8'));
 
 const args = process.argv.slice(2);
 
 if (args.length === 0) {
-    if (fs.existsSync(path.join(process.cwd(), '.saukko.sock'))) {
+    if (fs.existsSync(socketPath)) {
         logger.error('已存在 .saukko.sock 文件，框架可能已启动。');
         logger.notice('如果你确认框架未启动，请删除 .saukko.sock 文件后重试。');
         process.exit(1);
--- packages/saukko/src/types.ts
+++ packages/saukko/src/types.ts
@@ -1,5 +1,7 @@
 type SaukkoEnv = {
     SAUKKO_LOG_LEVEL?: 'trace' | 'debug' | 'info' | 'notice' | 'warn' | 'error' | 'silent';
+    SAUKKO_CONFIG_PATH?: string;
+    SAUKKO_SOCKET_PATH?: string;
 }
 
 export type { SaukkoEnv };
