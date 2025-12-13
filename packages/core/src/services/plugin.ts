54e71c23c4998e9c27c2a07483629e3afb8bd47c
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-13 12:31:45 +0800

    feat(core): basic pluginService type

--- packages/core/src/services/plugin.ts
+++ packages/core/src/services/plugin.ts
@@ -1,12 +1,39 @@
-import { Container } from "../container";
+import { Container, ServiceRegistry } from "../container";
 import { LoggerService } from "./logger";
+
+interface PluginType {
+    inject?: readonly (keyof ServiceRegistry)[];
+    name: string;
+    apply(context: any): void;
+    dispose?(): void | Promise<void>;
+}
+
+interface PluginMapItem {
+    name: string;
+    module: PluginType;
+    context: any;
+}
 
 export class PluginService {
     static inject = ['container', 'logger'] as const;
+    private plugins = new Map<string, PluginMapItem>();
 
     constructor(
         private container: Container,
         private logger: LoggerService
     ) {}
     
+    apply(pluginModule: PluginType) {
+        const dependencies = pluginModule.inject || [];
+        const injections = dependencies.map(dep => this.container.get(dep));
+        // WIP
+    }
+
+    remove(name: string) {
+        // WIP
+    }
+
+    list() {
+        return Array.from(this.plugins.keys());
+    }
 }
