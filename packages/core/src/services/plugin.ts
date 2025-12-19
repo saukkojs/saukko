28345eb23ee4407b0c540784c59a39eee37acca6
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-19 23:48:22 +0800

    feat(core): PluginContext & hard deps injection

--- packages/core/src/services/plugin.ts
+++ packages/core/src/services/plugin.ts
@@ -16,6 +16,12 @@ interface PluginMapItem {
     context: any;
 }
 
+class PluginContext {
+    public isEnabled: boolean = false;
+
+    constructor(public readonly depencies: PluginDepenciesRegistry) { }
+}
+
 export class PluginService {
     static inject = ['container', 'logger'] as const;
     private plugins = new Map<string, PluginMapItem>();
@@ -23,11 +29,24 @@ export class PluginService {
     constructor(
         private container: Container,
         private logger: LoggerService
-    ) {}
+    ) { }
     
     apply(pluginModule: PluginType) {
         const dependencies = pluginModule.inject || [];
-        const injections = dependencies.map(dep => this.container.get(dep));
+        const injections: Record<string, any> = {};
+        let missingDeps = [];
+        for (const dep of dependencies) {
+            if (!(this.container.has(dep))) {
+                missingDeps.push(dep);
+                continue;
+            }
+            injections[dep] = this.container.get(dep);
+        }
+        if (missingDeps.length > 0) {
+            this.logger.log('plugin', 'error', `Cannot apply plugin ${pluginModule.name}: depency ${missingDeps.join(', ')} not found`);
+            return;
+        }
+        const context = new PluginContext(injections);
         // WIP
     }
 
