bce0eb7a147799091d7218fa32cea280eb387777
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-20 13:06:47 +0800

    feat(core): basic EventEmitter for PluginContext

--- packages/core/src/services/plugin/context.ts
+++ packages/core/src/services/plugin/context.ts
@@ -2,7 +2,53 @@ import { PluginDepenciesRegistry } from "./types";
 
 export class PluginContext {
     public isEnabled: boolean = false;
+    private eventListeners: Map<string, Function[]> = new Map();
 
     constructor(public readonly depencies: PluginDepenciesRegistry) { }
-}
 
+    private disposeGenerator(event: string, listener: Function) {
+        return () => {
+            const listeners = this.eventListeners.get(event);
+            if (!listeners) return;
+            const index = listeners.indexOf(listener);
+            if (index !== -1) {
+                listeners.splice(index, 1);
+            }
+        }
+    }
+
+    on(event: string, listener: Function) {
+        if (!this.eventListeners.has(event)) {
+            this.eventListeners.set(event, []);
+        }
+        this.eventListeners.get(event)!.push(listener);
+
+        return this.disposeGenerator(event, listener);
+    }
+
+    once(event: string, listener: Function) {
+        const onceListener = (...args: any[]) => {
+            listener(...args);
+            dispose();
+        };
+        const dispose = this.on(event, onceListener);
+        return dispose;
+    }
+
+    off(event: string, listener: Function) {
+        const listeners = this.eventListeners.get(event);
+        if (!listeners) return;
+        const index = listeners.indexOf(listener);
+        if (index !== -1) {
+            listeners.splice(index, 1);
+        }
+    }
+
+    emit(event: string, ...args: any[]) {
+        const listeners = this.eventListeners.get(event);
+        if (!listeners) return;
+        for (const listener of listeners) {
+            listener(...args);
+        }
+    }
+}
