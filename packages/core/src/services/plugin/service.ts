0960e1d861f354e088aaf1823c76c0f81eb4d435
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-21 08:56:44 +0800

    refa(core): refactor PluginService API

--- packages/core/src/services/plugin/service.ts
+++ packages/core/src/services/plugin/service.ts
@@ -12,8 +12,9 @@ interface PluginType {
 interface PluginMapItem {
     name: string;
     module: PluginType;
-    context: PluginContext;
-    config: Record<string, any>;
+    context: PluginContext | undefined;
+    config: Record<string, any> | undefined;
+    enabled: boolean;
 }
 
 export class PluginService {
@@ -26,8 +27,44 @@ export class PluginService {
         private config: ConfigService
     ) { }
 
-    apply(pluginModule: PluginType) {
+    install(pluginModule: PluginType) {
+        if (this.plugins.has(pluginModule.name)) {
+            this.logger.log('plugin', 'error', `Plugin ${pluginModule.name} is already installed`);
+            this.logger.log('plugin', 'notice', 'In current version, creating multiple instances for a plugin is not supported.');
+            return;
+        }
         const dependencies = pluginModule.inject || [];
+        let missingDeps = [];
+        for (const dep of dependencies) {
+            if (!(this.container.has(dep))) {
+                missingDeps.push(dep);
+                continue;
+            }
+        }
+        if (missingDeps.length > 0) {
+            this.logger.log('plugin', 'warn', `Plugin ${pluginModule.name}: Dependency ${missingDeps.join(', ')} missing when intalling`);
+        }
+        this.plugins.set(pluginModule.name, {
+            name: pluginModule.name,
+            module: pluginModule,
+            context: undefined,
+            config: undefined,
+            enabled: false
+        });
+        this.logger.log('plugin', 'info', `+ ${pluginModule.name}`);
+    }
+
+    apply(name: string) {
+        const plugin = this.plugins.get(name);
+        if (!plugin) {
+            this.logger.log('plugin', 'error', `Cannot apply plugin ${name}: not found`);
+            return;
+        }
+        if (plugin.enabled) {
+            this.logger.log('plugin', 'error', `Plugin ${name} is already applied`);
+            return;
+        }
+        const dependencies = plugin.module.inject || [];
         const injections: Record<string, any> = {};
         let missingDeps = [];
         for (const dep of dependencies) {
@@ -38,21 +75,35 @@ export class PluginService {
             injections[dep] = this.container.get(dep);
         }
         if (missingDeps.length > 0) {
-            this.logger.log('plugin', 'error', `Cannot apply plugin ${pluginModule.name}: Dependency ${missingDeps.join(', ')} not found`);
+            this.logger.log('plugin', 'error', `Cannot apply plugin ${name}: Dependency ${missingDeps.join(', ')} not found`);
             return;
         }
         const pluginConfig = (this.config.get('plugin.config') as Record<string, any>) || {};
-        const currentConfig = pluginConfig[pluginModule.name] || {};
+        const currentConfig = pluginConfig[name] || {};
         const context = new PluginContext(injections, currentConfig);
-        pluginModule.default(context);
-        this.plugins.set(pluginModule.name, {
-            name: pluginModule.name,
-            module: pluginModule,
+        plugin.module.default(context);
+        this.plugins.set(name, {
+            ...plugin,
             context,
-            config: currentConfig
+            config: currentConfig,
+            enabled: true
         });
         context.emit('internal.ready', {});
-        this.logger.log('plugin', 'info', `+ ${pluginModule.name}`);
+        this.logger.log('plugin', 'info', `A ${name}`);
+    }
+
+    dispose(name: string) {
+        const plugin = this.plugins.get(name);
+        if (!plugin) {
+            this.logger.log('plugin', 'error', `Cannot dispose plugin ${name}: not found`);
+            return;
+        }
+        if (!plugin.enabled) {
+            this.logger.log('plugin', 'error', `Plugin ${name} is not enabled, dispose skipped`);
+            return;
+        }
+        plugin.context!.emit('internal.dispose', {});
+        this.logger.log('plugin', 'info', `D ${name}`);
     }
 
     remove(name: string) {
@@ -61,7 +112,6 @@ export class PluginService {
             this.logger.log('plugin', 'error', `Cannot remove plugin ${name}: not found`);
             return;
         }
-        plugin.context.emit('internal.dispose', {});
         this.plugins.delete(name);
         this.logger.log('plugin', 'info', `- ${name}`);
     }
