03cb988f51d6c8e70ecb7a13b17467fd7d6efc37
刘lyxAndy <liuyuxuanandy@outlook.com>
2025-12-21 12:32:00 +0800

    feat(core): bot emit api

--- packages/core/src/services/plugin/bot.ts
+++ packages/core/src/services/plugin/bot.ts
@@ -1,5 +1,13 @@
 import { PluginContext } from "./context";
+import { Events } from "./types";
 
 export abstract class Bot {
     constructor(public context: PluginContext) { }
+    protected emit<T extends keyof Events>(name:T, data: Events[T]) {
+        this.context.emit(name, {
+            name,
+            data,
+            bot: this
+        })
+    }
 }
