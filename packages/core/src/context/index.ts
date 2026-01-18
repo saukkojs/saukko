41659a0beaaf4e734b0208789cf5c5e316e4c17b
刘lyxAndy <liuyuxuanandy@outlook.com>
2026-01-18 09:49:45 +0800

    chore(core): export `Plugin`

--- packages/core/src/context/index.ts
+++ packages/core/src/context/index.ts
@@ -1,5 +1,6 @@
 import { Context } from "./context";
 import { Service } from "./service";
 import { Events } from "./events";
+import { Plugin } from "./plugin";
 
-export { Context, Service, type Events };
+export { Context, Service, type Events, type Plugin };
